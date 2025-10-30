c8c88ba5c47a075656be51da9b5e9d9438e84df3
serglo06 <serglo06@ucm.es>
2025-10-30 12:52:36 +0100

    clase combat setup

---
+++ docs/src/scenes/combatSetup.js
@@ -0,0 +1,22 @@
+export default class CombatSetup extends Phaser.Scene {
+
+    constructor() {
+        super({key: 'combatSetup'});
+    }
+
+    init() {
+
+    }
+
+    preload() {
+
+    }
+
+    create() {
+
+    }
+
+    update(time, deltaTime) {
+        
+    }
+}
