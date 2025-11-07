7e558df7c8ce885795a3a876d486520000174eae
zack <isserr01@ucm.es>
2025-11-07 07:05:52 +0100

    intento 3

--- docs/src/managers/DOMManager.js
+++ docs/src/managers/DOMManager.js
@@ -1,4 +1,4 @@
-import globalAlly from "../managers/globalAlly.js";
+import GlobalAlly from "../managers/globalAlly.js";
 
 /*
     Esta clase está destinada a controlar el DOM en tiempo de ejecución
