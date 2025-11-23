b1af4ae0f2490405d15b738959f197a93cb8961b
serglo06 <serglo06@ucm.es>
2025-11-23 19:59:39 +0100

    Mapa va por fin ;_;

--- docs/src/managers/binTree.js
+++ docs/src/managers/binTree.js
@@ -2,7 +2,7 @@
 
 class Node {
     // Nodo padre
-    father;
+    parent;
     // Valor del nodo
     value;
     // Nivel del nodo
@@ -16,10 +16,10 @@ class Node {
     // Botón
     button;
 
-    constructor(value = null, level = null, father = null, active = false) {
+    constructor(value = null, level = null, parent = null, active = false) {
         this.value = value;
         this.level = level;
-        this.father = father;
+        this.parent = parent;
         this.active = active;
         this.left = null;
         this.right = null;
@@ -48,32 +48,26 @@ export default class BinTree {
         this.buildTree(this.root, numLevels, i);
     }
 
-    buildTree(father, numLevels, it) {
+    buildTree(parent, numLevels, it) {
         it++;
         if(it > numLevels) {
-            father.left = new Node(null, null, father);
-            father.right = new Node(null, null, father);
+            parent.left = new Node(null, null, parent);
+            parent.right = new Node(null, null, parent);
             return;
         }
         
         //let val = Math.floor(Math.random() * 2);
         let val = 1;
         
-        father.left = new Node(val, it, father);
-        this.buildTree(father.left, numLevels, it);
+        parent.left = new Node(val, it, parent);
+        this.buildTree(parent.left, numLevels, it);
         
         //val = Math.floor(Math.random() * 2);
-        val = 0;
+        val = 1;
 
-        father.right = new Node(val, it, father);
-        this.buildTree(father.right, numLevels, it);
+        parent.right = new Node(val, it, parent);
+        this.buildTree(parent.right, numLevels, it);
 
         this.numNodes += 2;
-    }
-
-    debug() {
-        console.log(this.root.value);
-        console.log(this.root.left.value);
-        console.log(this.root.right.value);
     }
 }
