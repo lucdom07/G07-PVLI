29990e3676dc816191127c24fadb082f17cb2683
zack <isserr01@ucm.es>
2025-12-08 14:37:00 +0100

    mejoras visuales en el menú de inicio
    perdón chicas estoy loca

--- docs/src/scenes/mainMenu.js
+++ docs/src/scenes/mainMenu.js
@@ -22,8 +22,9 @@ export default class mainMenu extends Phaser.Scene{
     preload(){
         this.load.image('startButton','assets/placeholders/buttons/start_button.png')
         //this.load.image('marketButton','assets/button.png')
-        this.load.image('mainMenuBackground','assets/main_menu/background.png')
+        this.load.image('bg','assets/backgrounds/mainMenu.png')
         this.load.image('mainMenuCat','assets/main_menu/mainmenu_cat.png')
+        this.load.image('furry','assets/main_menu/furrylogo.png')
     }
 
     create(){
@@ -31,9 +32,32 @@ export default class mainMenu extends Phaser.Scene{
         this.audioManager.playMusic(MusicKeys.MENU);
 
         //fondo con título
-        this.add.image(this.sys.game.canvas.width*0.5, this.sys.game.canvas.height*0.5,'mainMenuBackground').setDisplaySize(this.sys.game.canvas.width,this.sys.game.canvas.height);
-        this.add.image(this.sys.game.canvas.width*0.17,this.sys.game.canvas.height*0.5,'mainMenuCat').setDisplaySize(450,550);
-   
+        this.add.image(this.sys.game.canvas.width*0.5, this.sys.game.canvas.height*0.5,'bg').setDisplaySize(this.sys.game.canvas.width,this.sys.game.canvas.height);
+        const cat = this.add.image(this.sys.game.canvas.width*0.25,this.sys.game.canvas.height*0.6,'mainMenuCat').setDisplaySize(550,710);
+        const meow = this.add.image(this.sys.game.canvas.width*0.7,this.sys.game.canvas.height*0.4,'furry').setDisplaySize(600,320);
+
+        this.tweens.add({
+        targets: cat,           
+        y: cat.y - 30,    
+        x: cat.x - 10 ,    
+        duration: 1800,         
+        ease: 'Sine.easeInOut', 
+        yoyo: true,             
+        loop: -1                
+        });
+
+
+        this.tweens.add({
+        targets: meow,           
+        y: meow.y - 50,          
+        duration: 1500,         
+        ease: 'Sine.easeInOut', 
+        yoyo: true,             
+        loop: -1                
+        });
+
+        
+
         //botón de juego
         const playButton = this.add.image(300, 100, 'startButton').setInteractive();
         //const marketButton = this.add.image(600,100,'marketButton').setInteractive();
